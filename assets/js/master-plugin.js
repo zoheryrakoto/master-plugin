a6b2d136f9c2c4958e15df4d34796f2d7388d0e8
zoheryrakoto <albert.zohery.rakoto@gmail.com>
2026-05-27 21:57:00 +0300

    Style: apply 2-space indentation

--- assets/js/master-plugin.js
+++ assets/js/master-plugin.js
@@ -1,19 +1,19 @@
 /* Master Plugin – Frontend Scripts */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    $(document).ready(function () {
+  $(document).ready(function () {
 
-        // Example: AJAX ping on page load (remove or adapt as needed)
-        $.post(masterPluginData.ajaxUrl, {
-            action: 'master_plugin_action',
-            nonce:  masterPluginData.nonce,
-        }, function (response) {
-            if (response.success) {
-                console.log('[Master Plugin]', response.data.message);
-            }
-        });
-
+    // Example: AJAX ping on page load (remove or adapt as needed)
+    $.post(masterPluginData.ajaxUrl, {
+      action: 'master_plugin_action',
+      nonce:  masterPluginData.nonce,
+    }, function (response) {
+      if (response.success) {
+        console.log('[Master Plugin]', response.data.message);
+      }
     });
+
+  });
 
 }(jQuery));
--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 // Only run when WordPress is uninstalling the plugin.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
+  exit;
 }
 
 delete_option( 'master_plugin_options' );
